d62a1b7db559ea8a182fea6b42dd570858e7d01d
Carlos Machel <carlos.git@outlook.com>
2018-03-26 19:04:56 -0300

    adding BEM to css

--- portifolio/js/main.js
+++ portifolio/js/main.js
@@ -1,18 +1,18 @@
 (function(win, doc){
   'use strict';
 
-  var $menu = doc.querySelector('[data-js="menu-sandwich"]');
-  var $nav = doc.querySelector('[role="navigation"]');
+  var $menu = doc.querySelector('[data-js="js-header__sandwich"]');
+  var $nav = doc.querySelector('[data-js="js-nav"]');
   
   $menu.addEventListener('click', toggle, false);
 
   function toggle(event) {
     event.preventDefault();
     
-    if ($nav.classList.contains('nav-active') ) {
-      $nav.classList.remove('nav-active');      
+    if ($nav.classList.contains('nav--active') ) {
+      $nav.classList.remove('nav--active');      
     } else {
-      $nav.classList.add('nav-active');
+      $nav.classList.add('nav--active');
     }
   }
 
